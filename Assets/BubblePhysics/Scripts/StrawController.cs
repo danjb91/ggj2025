8ec3044f3665a7233088612e8ee6d60ad8a2a63c
Raphael Priatama <raphaelpriatama@gmail.com>
2025-01-25 17:09:55 +0100

    straw screen limits

--- Assets/BubblePhysics/Scripts/StrawController.cs
+++ Assets/BubblePhysics/Scripts/StrawController.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class StrawController : MonoBehaviour
 {
-    [SerializeField] private Transform strawLimiter;
+    [SerializeField] private Vector3 strawLimiter;
+    [SerializeField] private Vector3 lowerLeft;
+    [SerializeField] private Vector3 upperRight;
+
     [SerializeField] private float strawMoveSpeed = 1.0f;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Vector3 movement;
@@ -11,8 +14,18 @@ public class StrawController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Resolution res = Screen.currentResolution;
+        print(res);
+
+        strawLimiter = GameObject.FindGameObjectWithTag("StrawYLimit").transform.position;
+        Vector3 cameraPos = Camera.main.transform.position;
+
+        float distFromCam = strawLimiter.x - cameraPos.x;
+        
+        lowerLeft = Camera.main.ScreenToWorldPoint(new Vector3(50, 0, distFromCam) );
+        upperRight = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth-50, Camera.main.pixelHeight, distFromCam));
+
         rb = gameObject.GetComponent<Rigidbody>();
-        strawLimiter = GameObject.FindGameObjectWithTag("StrawYLimit").transform;
     }
 
     // Update is called once per frame
@@ -22,7 +35,7 @@ public class StrawController : MonoBehaviour
 
         if (connectToWasd)
         {
-            movement.z = -Input.GetAxis("Horizontal");
+            movement.z = Input.GetAxis("Horizontal");
             movement.y = Input.GetAxis("Vertical");
             movement.Normalize();
 
@@ -31,16 +44,36 @@ public class StrawController : MonoBehaviour
             resultingMovement = movement * strawMoveSpeed; ;
         
 
-            if(movement.y < 0.0f && transform.position.y < strawLimiter.transform.position.y)
+            if(movement.y < 0.0f && transform.position.y < strawLimiter.y)
             {
                 resultingMovement.y = 0.0f;
-                transform.position = new Vector3(transform.position.x, strawLimiter.transform.position.y, transform.position.z);
+                transform.position = new Vector3(transform.position.x, strawLimiter.y, transform.position.z);
+            }
+            else if(movement.y > 0.0f && transform.position.y > upperRight.y)
+            {
+                resultingMovement.y = 0.0f;
+                transform.position = new Vector3(transform.position.x, upperRight.y, transform.position.z);
+            }
+            else if (movement.z < 0.0f && transform.position.z > lowerLeft.z)
+            {
+                resultingMovement.z = 0.0f;
+                transform.position = new Vector3(transform.position.x, transform.position.y, lowerLeft.z);
+            }
+            else if (movement.z > 0.0f && transform.position.z < upperRight.z)
+            {
+                resultingMovement.z = 0.0f;
+                transform.position = new Vector3(transform.position.x, transform.position.y, upperRight.z);
             }
 
+            //Debug.Log("pos  " + transform.position.z + " left " + lowerLeft.z);
 
-       
         }
 
+        //Debug.Log("move " + resultingMovement);
+
+        resultingMovement.z *= -1;
+
+        //Debug.Log("after move " + resultingMovement);
         rb.linearVelocity = resultingMovement;
     }
 }
