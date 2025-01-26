96edc19b0406e70b5af744f210b9f6db0a568453
fnox <thefnox@gmail.com>
2025-01-26 13:51:50 +0100

    fixed straw logic

--- Assets/BubblePhysics/Scripts/StrawScaleManager.cs
+++ Assets/BubblePhysics/Scripts/StrawScaleManager.cs
@@ -6,14 +6,14 @@ public class StrawScaleManager : MonoBehaviour
     public static float maxMoneyRange = 1000.0f;
 
     public static float minScale = 1.0f;
-    public static float maxScale = 4.0f;
+    public static float maxScale = 3.0f;
 
     public int owner = 0;
     private void LateUpdate()
     {
         float money = Mathf.Max(minMoneyRange, (float)GameManager.Instance.stockSim.currentMoney[owner]);
         float moneyRatio = money / maxMoneyRange;
-        float scale = minScale + (maxScale - minScale) * moneyRatio;
+        float scale = Mathf.Min(maxScale, minScale + (maxScale - minScale) * moneyRatio);
 
         gameObject.transform.localScale = new Vector3(scale, gameObject.transform.localScale.y, scale);
 
